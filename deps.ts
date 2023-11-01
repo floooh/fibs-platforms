92b5a0d7393a00e41ce650b17e95785e389e3462
Andre Weissflog <floooh@gmail.com>
2023-11-01 15:22:01 +0100

    fix fibs import url

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as fibs from 'https://raw.githubusercontent.com/floooh/fibs/main/mod.ts';
+export * as fibs from 'https://deno.land/x/fibs@v1.0.0/mod.ts';
 export * as colors from 'https://deno.land/std@0.178.0/fmt/colors.ts';
