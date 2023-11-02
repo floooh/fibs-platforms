2345a020ed33d2a5686cfd9b00d362fd07a2b4a4
Andre Weissflog <floooh@gmail.com>
2023-11-02 11:48:17 +0100

    bump fibs to v1.2.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as fibs from 'https://deno.land/x/fibs/mod.ts';
+export * as fibs from 'https://deno.land/x/fibs@v1.2.0/mod.ts';
 export * as colors from 'https://deno.land/std@0.204.0/fmt/colors.ts';
 export { default as WASI } from 'https://deno.land/std@0.204.0/wasi/snapshot_preview1.ts'
